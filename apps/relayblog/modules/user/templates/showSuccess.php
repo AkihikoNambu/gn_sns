560f1e2003518b130a30063ba171f2c6ac45608c
Murakami Shumpei <pei.shum1227@gmail.com>
2014-06-27 20:07:10 +0900

    mypage b

--- apps/relayblog/modules/user/templates/showSuccess.php
+++ apps/relayblog/modules/user/templates/showSuccess.php
@@ -1,16 +1,38 @@
-<div id = 'add_imfomation'>
-	<tr>
-		<td><h4>UserName</h4></td>
-		<td><?php echo $subscriber->getUserName() ?></td>
-	</tr>
-		<td><?php echo $subscriber->getFirstName() ?></td>
-		<td><?php echo $subscriber->getMiddleName ?></td>
-		<td><?php echo $subscriber->getLastName ?></td>
-		<td><?php echo $subscriber->getEmail ?></td>
-	</tr>
+<div class = 'user_infomation'>	
+	<div id = 'add_infomation'>
+		<table>
+			<tr>
+				<th>UserName: </th>
+				<td><?php echo $subscriber->getUserName() ?></td>
+			</tr>
+			<tr>
+				<th>FirstName: </th>
+				<td><?php echo $subscriber->getFirstName() ?></td>
+			</tr>
+			<tr>
+				<th>MiddleName: </th>
+				<td><?php echo $subscriber->getMiddleName() ?></td>
+			</tr>
+			<tr>
+				<th>LastName: </th>
+				<td><?php echo $subscriber->getLastName() ?></td>
+			</tr>
+			<tr>
+				<th>e-mail: </th>
+				<td><?php echo $subscriber->getEmail() ?></td>
+			</tr>
+		</table>
+	</div>
+	<div id = 'user_image'>
+		<?php if($subscriber->getImage() != 'Null'): ?>
+			<h3>No Image</h3>
+		<?php else: ?>
+			<?php echo $subscriber->getImage() ?>
+		<?php endif ?>
+	</div>
 </div>
 
-<div id = 'contribution_imfomation'>
+<div id = 'contribution_infomation'>
 
 
 </div>
